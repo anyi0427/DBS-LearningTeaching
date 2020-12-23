107ad3578b07f205fda8bcd720539d812229287a
An Yi <anyiikb@gmail.com>
2020-12-23 16:57:01 +0700

    ReConstruct-Project

--- Webapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'home'
-urlpatterns = [
-   path('', views.Form),
-   path('saveEvaluateForm/', views.saveEvaluateForm, name = "saveEvaluateForm"),
-   path('list/', views.SinhVien),
-]
